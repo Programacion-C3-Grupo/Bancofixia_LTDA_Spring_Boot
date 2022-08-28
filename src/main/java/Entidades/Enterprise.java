df6bf5540b8e476975e71855287b80d6a6a232c2
JOTA <correo@gmail.com>
2022-08-28 09:22:19 -0500

    Agrego clase Enterprise

--- src/main/java/Entidades/Enterprise.java
+++ src/main/java/Entidades/Enterprise.java
@@ -1,2 +1,53 @@
-package Entidades;public class Enterprise {
+package Entidades;
+
+import javax.persistence.*;
+
+@Entity
+public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String document;
+    private String phone;
+    private String address;
+    @OneToMany
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    public Enterprise(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
