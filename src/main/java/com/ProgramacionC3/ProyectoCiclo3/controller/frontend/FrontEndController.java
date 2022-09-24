7f785a7c4559388456376813add6ac64720bf36c
ricomariaca <61256274+ricomariaca@users.noreply.github.com>
2022-09-24 11:58:33 -0500

    ss

--- src/main/java/com/ProgramacionC3/ProyectoCiclo3/controller/frontend/FrontEndController.java
+++ src/main/java/com/ProgramacionC3/ProyectoCiclo3/controller/frontend/FrontEndController.java
@@ -40,21 +40,5 @@ public class FrontEndController {
         model.addAttribute("employee", employeeService.listar());
         return "employeetable";
     }
-//=======
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
 
-//@Controller
-
-//public class FrontEndController {
-
-    //@GetMapping("/")
-    //public String getIndex(){
-        //return "index";
-
-    //}
-
-
-
-//>>>>>>> Stashed changes
 }
