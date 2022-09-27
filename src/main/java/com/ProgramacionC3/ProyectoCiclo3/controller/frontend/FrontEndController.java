393c18d96f44c53e199c1eb47b23f9fdc0df1161
Daniel Galvis Reyes <danielgalvisr@hotmail.com>
2022-09-26 22:25:04 -0500

    Mas cambios por que me devolvieron los mios

--- src/main/java/com/ProgramacionC3/ProyectoCiclo3/controller/frontend/FrontEndController.java
+++ src/main/java/com/ProgramacionC3/ProyectoCiclo3/controller/frontend/FrontEndController.java
@@ -2,6 +2,7 @@ package com.ProgramacionC3.ProyectoCiclo3.controller.frontend;
 
 //<<<<<<< Updated upstream
 
+import com.ProgramacionC3.ProyectoCiclo3.controller.EmployeeController;
 import com.ProgramacionC3.ProyectoCiclo3.controller.EnterpriseController;
 import com.ProgramacionC3.ProyectoCiclo3.controller.TransactionController;
 import com.ProgramacionC3.ProyectoCiclo3.entities.Employee;
@@ -35,6 +36,8 @@ public class FrontEndController {
     @Autowired
     TransactionController transactionController = new TransactionController();
 
+    @Autowired
+    EmployeeController employeeController = new EmployeeController();
 
 
 
@@ -53,7 +56,7 @@ public class FrontEndController {
     @PostMapping("/empleado")
     public String postEmployee(@ModelAttribute("formularioEmpleado") Employee employee)
     {
-       /* EmployeeController.insertar(employee);*/
+        employeeController.insertar(employee);
         System.out.println(employee);
         return "redirect:/empleado";
     }
