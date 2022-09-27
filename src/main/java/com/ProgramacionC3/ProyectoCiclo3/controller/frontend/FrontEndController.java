8f700e61e6d5f5752aaa09b25ab6f6481ae76864
JOTA <correo@gmail.com>
2022-09-26 22:00:12 -0500

    Se modifica FrontEndController, se crea get y post

--- src/main/java/com/ProgramacionC3/ProyectoCiclo3/controller/frontend/FrontEndController.java
+++ src/main/java/com/ProgramacionC3/ProyectoCiclo3/controller/frontend/FrontEndController.java
@@ -2,10 +2,13 @@ package com.ProgramacionC3.ProyectoCiclo3.controller.frontend;
 
 //<<<<<<< Updated upstream
 
+import com.ProgramacionC3.ProyectoCiclo3.controller.EnterpriseController;
 import com.ProgramacionC3.ProyectoCiclo3.controller.TransactionController;
 import com.ProgramacionC3.ProyectoCiclo3.entities.Employee;
+import com.ProgramacionC3.ProyectoCiclo3.entities.Enterprise;
 import com.ProgramacionC3.ProyectoCiclo3.entities.Transaction;
 import com.ProgramacionC3.ProyectoCiclo3.service.EmployeeService;
+import com.ProgramacionC3.ProyectoCiclo3.service.EnterpriseService;
 import com.ProgramacionC3.ProyectoCiclo3.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,6 +25,12 @@ public class FrontEndController {
 
     @Autowired
     TransactionService transactionService;
+
+    @Autowired
+    EnterpriseService enterpriseService;
+
+    @Autowired
+    EnterpriseController enterpriseController = new EnterpriseController();
 
     @Autowired
     TransactionController transactionController = new TransactionController();
@@ -80,6 +89,28 @@ public class FrontEndController {
     public String TransactionTable(Model model){
         model.addAttribute("Transactions", transactionService.listar());
         return "TransactionTable";
+    }
+
+    //------------Empresa-------------
+    @GetMapping("/empresa")
+    public String getEmpresa(Model model)
+    {
+        model.addAttribute("formularioEmpresa",new Enterprise());
+        return "empresa" ;
+    }
+    @PostMapping("/empresa")
+    public String postEmpresa(@ModelAttribute("formularioEmpresa")Enterprise enterprise)
+    {
+
+        enterpriseController.insertar(enterprise);
+        System.out.println(enterprise);
+        return "redirect:/empresaTable";
+    }
+
+    @GetMapping("/empresaTable")
+    public String empresaTable(Model model){
+        model.addAttribute("empresas", enterpriseController.listar());
+        return "empresaTable";
     }
 
 }
